867395c8ac48c378b594ba464c8dfa667d888447
alessio <almatteo@tiscali.it>
2018-10-11 17:21:13 +0200

    Added readme, .gitignore, c string example

--- main.c
+++ main.c
@@ -3,12 +3,23 @@
 
 int main()
 {
-    // changing first 2 bytes of uint32_t
+    // bytes substitution
+    printf("Changing first 2 bytes of uint32_t:\n");
     uint32_t res = 0xABCD1234;
-    printf("before %lX \n",res);
+    printf("before %08X \n",res);
     res &= 0x00FFFFFF;
     res |= 0x25000000;
-    printf("after %lX \n",res);
+    printf("after %08X \n",res);
+
+    printf("\n");
+
+    // c string example
+    printf("Printing \"hello\" string as char array:\n");
+    char hello_string[5] = "hello";
+    for (int i=0; i<(sizeof(hello_string)/sizeof(char)); i++) 
+    {
+      printf("char[%d] = %c\n",i,hello_string[i]);
+    }
 
     return 0;
 };
